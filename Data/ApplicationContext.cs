f2dd737c77ac53afb0d0928bebb91f938c698229
José Cristovão <j_cristovao@hotmail.com>
2023-05-09 16:54:52 +0100

    #Actualização

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtWebApi.Data
+{
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- Entity/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JwtWebApi.Entity
-{
-    public class User
-    {
-        public string UserName { get; set; } = string.Empty;
-        public byte[] Passwordhash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-    }
-}
